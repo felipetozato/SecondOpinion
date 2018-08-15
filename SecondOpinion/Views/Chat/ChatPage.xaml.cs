556ea37c06c8271ca15f23a1c52022b0b7cbb991
Felipe Cantagalli <fcantagalli@tiendanube.com>
2018-08-14 22:25:06 -0300

    Working in the ugly way

---
+++ SecondOpinion/Views/Chat/ChatPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Xamarin.Forms;
+using SecondOpinion.ViewModels;
+using SecondOpinion.Models;
+using ReactiveUI;
+using System.Reactive.Linq;
+using System.Collections.Specialized;
+using System.Reactive.Disposables;
+
+namespace SecondOpinion.Views.Chat {
+    public partial class ChatPage : ContentPageBase<DialogViewModel> {
+        public ChatPage (Dialog chat) : base() {
+            InitializeComponent();
+            ViewModel.Chat = chat;
+            ViewModel.Populate();
+            SubscribeToViewModel();
+        }
+
+        private void SubscribeToViewModel() {
+            this.WhenActivated(disposable => {
+                this.Bind(ViewModel , vm => vm.MessageText , v => v.MessageEditText.Text)
+                    .DisposeWith(SubscriptionDisposables);
+                this.BindCommand(ViewModel , vm => vm.SendMessageCommand , v => v.SendMessageButton)
+                    .DisposeWith(SubscriptionDisposables);
+                ViewModel.MessageList.Changed
+                         .ObserveOn(RxApp.MainThreadScheduler)
+                         .Subscribe(AddMessageToTableView);
+            });
+        }
+
+        private void AddMessageToTableView (NotifyCollectionChangedEventArgs eventArgs) {
+            this.MessageList.ItemsSource = ViewModel.MessageList.OrderBy(x => x.Id);
+        }
+    }
+}
--- SecondOpinion/Views/ChatList/DialogListPage.xaml.cs
+++ SecondOpinion/Views/ChatList/DialogListPage.xaml.cs
@@ -5,25 +5,40 @@ using Xamarin.Forms;
 using SecondOpinion.ViewModels;
 using ReactiveUI;
 using System.Reactive.Linq;
+using SecondOpinion.Models;
+using SecondOpinion.Views.Chat;
+using System.Reactive.Disposables;
 
 namespace SecondOpinion.Views.ChatList {
     public partial class DialogListPage : ContentPageBase<DialogListViewModel> {
         public DialogListPage () {
             InitializeComponent();
+            Init();
             ViewModel.Populate();
             SubscribeToViewModel();
 
             this.Title = "Conversas";
         }
 
+        void Init() {
+            this.WhenActivated(disposables => {
+                DialogList.Events().ItemSelected
+                          .Select(x => x.SelectedItem as Dialog)
+                          .Subscribe(dialog => {
+                    var chatPage = new ChatPage(dialog);
+                    this.Navigation.PushAsync(chatPage);
+                }).DisposeWith(SubscriptionDisposables);
+            });
+        }
+
         private void SubscribeToViewModel() {
             this.WhenActivated(disposables => {
                 ViewModel.ChatList.Changed
                          .Where(list => list != null && list.NewItems.Count > 0)
+                         .ObserveOn(RxApp.MainThreadScheduler)
                          .Subscribe(chatList => {
                     DialogList.ItemsSource = chatList.NewItems;
                 });
-
             });
         }
     }
